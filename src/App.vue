41d0c277a96e3cd80afcb5ee88730af4fbd535f9
DanieleTuttolani <danieletuttolani2@gmail.com>
2023-01-20 14:36:35 +0100

    section imported , sass added , css reset

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
 
+export default{
+  data(){
+    return{
+
+    }
+  }
+  ,
+  components:{
+    AppHeader,
+    AppMain,
+    AppFooter
+  }
+}
 </script>
 
 <template>
@@ -7,5 +23,18 @@
 </template>
 
 <style>
-
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: sans-serif;
+}
+/*generics */ 
+ul{
+  list-style-type: none;
+}
+img{
+  max-height: 100%;
+  width: auto;
+}
 </style>
