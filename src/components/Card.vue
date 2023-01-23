654098f74dfeacc2b40eff389251c63cfbe8cb06
DanieleTuttolani <danieletuttolani2@gmail.com>
2023-01-23 17:15:11 +0100

    dynamic list

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,39 +1,35 @@
 <script>
     export default{
-
+     props:{
+         comic: Object
+     }
     }
 </script>
 <template>
-    <div class="card-wrapper">
         <div class="card">
-             <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
-             <p>comic name </p>
+             <img :src="comic.thumb" :alt="comic.series">
+             <p>{{ comic.series }}</p>
         </div>
-        
-    
-    </div>
 </template>
 
 
 <style lang="scss" scoped>
-.card-wrapper{
-    display: flex;
-    flex-wrap: wrap;
+
     .card{
         flex-basis: calc(100% / 6);
         margin-bottom: 2rem;
         height: 200px;
-        img{
-            width: 200px;
-            object-fit: cover;
-            object-position: top;
-            padding: 0 10px;
-          }
-          p{
-               color: white;
-               padding: 0 10px;
-               margin: 10px 0
-          }
-    }
+     img{
+      width: 200px;
+     object-fit: cover;
+      object-position: top;
+      padding: 0 10px;
+     }
+      p{
+     color: white;
+     padding: 0 10px;
+     margin: 10px 0
+     }
 }
+
 </style>
